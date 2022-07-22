8e3fc9468c105a11f8f1bb37e482743c43557a64
Iwan1 <paddo144@gmail.com>
2022-07-22 03:37:09 +0000

    first_commit

---
+++ program.swift
@@ -0,0 +1,60 @@
+import Foundation
+import UIKit
+
+//varfiable
+var str = "Iwan becomes a coder in real life and bring his enemies to his knees"
+print(str)
+
+var num1 = 20
+print(num1)
+
+var num2 = 40 + 50
+print(num2)
+
+var num3 = 120
+
+print(num3)
+
+
+//static
+class Person{
+
+    static let age = 12
+    let lastName = "ADDO"
+
+}
+
+var Prince_Nathan = Person()
+var Mabel = Person()
+var Grace = Person()
+var Adoma = Person()
+var Chris = Person()
+var Antony = Person()
+
+let age = Person.age
+let ammout = 20.20
+var name = "Joe";
+name = "jill"
+
+
+
+//Types
+
+var a = 4 //integer
+var name = "King" // string
+
+var lastName: String = "liz"
+let age: Int = 33
+let ammount: Double = 59.44
+
+class Person{
+
+
+}
+var john: Person = Person()
+
+var button1: UIButton = UIButton()
+// Boolean
+var isComplete = false
+isComplete = true
+
